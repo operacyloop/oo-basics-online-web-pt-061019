72c26f88d2c002eda665989bca4f6f05391c3921
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-07 22:58:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -18,7 +18,7 @@ class Shoe
   
   def cobble
     self.condition = "new"	   
-    puts "Your shoe is as good as new!"	    puts "Your shoe is as good as new!"
-  end	  end
+    puts "Your shoe is as good as new!"	    
+  end
   
 end 
