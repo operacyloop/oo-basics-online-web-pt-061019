b46b51c9636d9af0dbd3c16d688503be1f2bbfd2
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-07 23:39:09 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe 
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader :brand
+  
+   def initialize(brand)
+     @brand = brand
+     # BRANDS << brand unless BRANDS.include?(brand)
+     # BRANDS == BRANDS.uniq!
+     # @brand = "Nike"
+   end
+
+  # def initialize(color)
+  #    @color = color
+  # end
+   
+    def cobble
+    self.condition = "new"	   
+    puts "Your shoe is as good as new!"	    
+  end
+  
+ # BRANDS  = []
+
+end 
