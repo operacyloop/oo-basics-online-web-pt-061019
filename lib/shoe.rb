8e5a2af26d3e5955a733a21aebaa84f8d1201a79
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-07 22:21:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe 
   attr_accessor :brand, :color, :size, :material, :condition
   
-# def initialize(brand)
-#  @brand = brand
-# end
+  def initialize(brand)
+    @brand = brand
+  end
 
   def initialize(color)
     @color = color
